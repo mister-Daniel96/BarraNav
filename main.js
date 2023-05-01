458c09f8ad96a6c8b80c45c7e007468aa947b137
Juan Ayala <juandanielayalaquinones@gmail.com>
2023-05-01 13:03:19 -0500

    BarraNav

---
+++ main.js
@@ -0,0 +1,12 @@
+bars = document.querySelector(".bars");
+
+bars.onclick = function () {
+  navBar = document.querySelector(".navBar");
+  navBar.classList.toggle("activar");
+};
+
+/* const bars = document.querySelector(".bars");
+bars.onclick = function () {
+  const navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+}; */
